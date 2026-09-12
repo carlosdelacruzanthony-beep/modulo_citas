f536630a98653b5d5ec29b6e5e94d9a3154314cc
lumionz1 <carlosdelacruzanthony@gmail.com>
2026-09-12 17:43:18 -0500

    arreglo conflicto

--- citas.py
+++ citas.py
@@ -10,6 +10,7 @@ def cancelar_cita(paciente):
     global citas_programadas
     citas_programadas = [c for c in citas_programadas if c["paciente"] != paciente]
     print(f"Citas de {paciente} canceladas.")
+<<<<<<< HEAD
     
 def buscar_cita(paciente):
     """Busca y muestra todas las citas programadas para un paciente."""
@@ -21,3 +22,15 @@ def buscar_cita(paciente):
             print(f"- Fecha: {cita['fecha']} a las {cita['hora']}")
     else:
         print(f"\nNo se encontraron citas para {paciente}.")
+=======
+
+def vuscar_sita(paciente):
+    citas_encontradas = [c for c in citas_programadas if c["paciente"] == paciente]
+
+    if sitas_encontradas:
+        print(f"Citas encontradas para {paciente}:")
+        for cita in citas_encontradas:
+            print(f"Fecha: {cita['fecha']}, Hora: {cita['hora']}")
+    else:
+        print(f"No se encontraron sitas para {paciente}.")
+>>>>>>> 7223af8 (metodo buscar citas mal hecho)
